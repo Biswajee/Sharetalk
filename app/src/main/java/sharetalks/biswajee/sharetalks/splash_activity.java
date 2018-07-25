b5c90f5362829cf926c65ae227b0a2c685c3ddb6
Biswajee <roy.biswajeet161@gmail.com>
2018-07-25 20:04:08 +0530

    Done adding splash screen

---
+++ app/src/main/java/sharetalks/biswajee/sharetalks/splash_activity.java
@@ -0,0 +1,28 @@
+package sharetalks.biswajee.sharetalks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Biswajit Roy on 25-07-2018.
+ */
+
+public class splash_activity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(splash_activity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
